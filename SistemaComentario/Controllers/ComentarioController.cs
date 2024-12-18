57e44370a4254814050c28317250f439f7fd88bc
VictorAugustoRS <victorsantos_99@hotmail.com>
2024-12-18 03:34:04 -0300

    Criar, editar e excluir uma resposta / Finalização e correçao de erros

--- SistemaComentario/Controllers/ComentarioController.cs
+++ SistemaComentario/Controllers/ComentarioController.cs
@@ -8,7 +8,8 @@ namespace SistemaComentario.Controllers
 {
     public class ComentarioController : Controller
     {
-        private static List<Comentario> comentarios = new List<Comentario>();
+        public static List<Comentario> comentarios = new List<Comentario>();
+        public static List<Resposta> respostas = new List<Resposta>();
         public ActionResult Index()
         {
             return View(comentarios);
@@ -25,7 +26,7 @@ namespace SistemaComentario.Controllers
         {
             if (ModelState.IsValid)
             {
-                comentario.Id = comentarios.Count + 1;
+                comentario.Id = comentarios.Count > 0 ? comentarios.Max(c => c.Id) + 1 : 1;
                 comentario.DataPost = DateTime.Now;
                 comentarios.Add(comentario);
                 return RedirectToAction("Index");
@@ -37,9 +38,13 @@ namespace SistemaComentario.Controllers
         {
             var comentario = comentarios.FirstOrDefault(c => c.Id == id);
             if (comentario == null)
+            {
                 return HttpNotFound();
+            }
             return View(comentario);
         }
+
+
 
         [HttpGet]
         public ActionResult Editar(int id)
@@ -70,6 +75,29 @@ namespace SistemaComentario.Controllers
                 comentarios.Remove(comentario);
             }
             return RedirectToAction("Index");
+        }
+        public ActionResult AdicionarResposta(int comentarioId)
+        {
+            var resposta = new Resposta { ComentarioId = comentarioId };
+            return View(resposta);
+        }
+
+        [HttpPost]
+        public ActionResult AdicionarResposta(Resposta resposta)
+        {
+            if (ModelState.IsValid)
+            {
+                var comentario = comentarios.FirstOrDefault(c => c.Id == resposta.ComentarioId);
+                if (comentario != null)
+                {
+                    resposta.Id = comentario.Respostas.Count > 0 ? comentario.Respostas.Max(r => r.Id) + 1 : 1;
+                    resposta.DataPost = DateTime.Now;
+                    comentario.Respostas.Add(resposta);
+                }
+                return RedirectToAction("Detalhes", new { id = resposta.ComentarioId });
+            }
+
+            return View(resposta);
         }
 
     }
---
+++ SistemaComentario/Controllers/RespostaController.cs
@@ -0,0 +1,87 @@
+﻿using SistemaComentario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaComentario.Controllers
+{
+    public class RespostaController : Controller
+    {
+        public static List<Comentario> comentarios = ComentarioController.comentarios;
+
+        [HttpGet]
+        public ActionResult Criar(int comentarioId)
+        {
+            var comentario = comentarios.FirstOrDefault(c => c.Id == comentarioId);
+            if (comentario == null)
+            {
+                return HttpNotFound();
+            }
+            var resposta = new Resposta { ComentarioId = comentarioId };
+            return View(resposta);
+        }
+
+        [HttpPost]
+        public ActionResult Criar(Resposta resposta)
+        {
+            if (ModelState.IsValid)
+            {
+                var comentario = comentarios.FirstOrDefault(c => c.Id == resposta.ComentarioId);
+                if (comentario != null)
+                {
+                    resposta.Id = comentario.Respostas.Count > 0 ? comentario.Respostas.Max(r => r.Id) + 1 : 1;
+                    resposta.DataPost = DateTime.Now;
+                    comentario.Respostas.Add(resposta);
+                }
+                return RedirectToAction("Detalhes", "Comentario", new { id = resposta.ComentarioId });
+            }
+            return View(resposta);
+        }
+
+        [HttpGet]
+        public ActionResult Editar(int id, int comentarioId)
+        {
+            var comentario = comentarios.FirstOrDefault(c => c.Id == comentarioId);
+            if (comentario == null)
+                return HttpNotFound();
+
+            var resposta = comentario.Respostas.FirstOrDefault(r => r.Id == id);
+            if (resposta == null)
+                return HttpNotFound();
+
+            return View(resposta);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Resposta resposta)
+        {
+            var comentario = comentarios.FirstOrDefault(c => c.Id == resposta.ComentarioId);
+            if (comentario != null)
+            {
+                var respostaExistente = comentario.Respostas.FirstOrDefault(r => r.Id == resposta.Id);
+                if (respostaExistente != null && ModelState.IsValid)
+                {
+                    respostaExistente.Texto = resposta.Texto;
+                    return RedirectToAction("Detalhes", "Comentario", new { id = resposta.ComentarioId });
+                }
+            }
+            return View(resposta);
+        }
+
+        public ActionResult Excluir(int id, int comentarioId)
+        {
+            var comentario = comentarios.FirstOrDefault(c => c.Id == comentarioId);
+            if (comentario != null)
+            {
+                var resposta = comentario.Respostas.FirstOrDefault(r => r.Id == id);
+                if (resposta != null)
+                {
+                    comentario.Respostas.Remove(resposta);
+                }
+            }
+            return RedirectToAction("Detalhes", "Comentario", new { id = comentarioId });
+        }
+    }
+}
